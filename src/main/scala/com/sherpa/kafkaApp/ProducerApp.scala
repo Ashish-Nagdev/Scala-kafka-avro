06121f1f58fe58069e5b1c8b4f979fed881d430b
Ashish_Nagdev <ashunagdev@gmail.com>
2017-01-09 04:26:39 -0500

    Kafka Producer-Consumer App

---
+++ src/main/scala/com/sherpa/kafkaApp/ProducerApp.scala
@@ -0,0 +1,17 @@
+package com.sherpa.kafkaApp
+
+import com.sherpa.producer.KafkaProducer
+import domain.User
+
+object ProducerApp extends App {
+
+  private val topic = "demo-topic"
+
+  val producer = new KafkaProducer()
+
+  val user1 = User(1, "Ashish Nagdev", None)
+  val user2 = User(2, "Ashish Nagdev", Some("ashish@sherpa.com"))
+
+  producer.send(topic, List(user1, user2))
+
+}
